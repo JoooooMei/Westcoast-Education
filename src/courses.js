44afdc1d2c5a4326a6402a99ecad5478aafc9a7e
JoooooMei <c.johan.meisel@gmail.com>
2025-01-29 18:59:44 +0100

    filter popular courses

--- src/courses.js
+++ src/courses.js
@@ -13,6 +13,9 @@ const findCourses = async () => {
     if (response.ok) {
       const courses = await response.json();
 
+      const popularCourses = courses.filter((course) => course.popular);
+      console.log(popularCourses);
+      // const classroomCourses = db.utbildningar.filter(course => course.classRoom);
       showAllCourses(courses);
     } else {
       throw new Error(response.status.toString());
@@ -21,33 +24,3 @@ const findCourses = async () => {
     console.log(error);
   }
 };
-
-const loadVehicles = async () => {
-  const url = 'http://localhost:3000/vehicles';
-  const response = await fetch(url); //GET ANROP...
-
-  if (response.ok) {
-    const vehicles = await response.json(); //Hämta ut data ur body...
-    displayVehicles(vehicles);
-  }
-};
-
-// const findMovie = async (): Promise<void> => {
-//   const id = location.search.split('=')[1];
-//   const key = 'c225640b9109317dc84c9f661f0ca0ba';
-//   const url = `https://api.themoviedb.org/3/movie/${id}?api_key=${key}`;
-
-//   try {
-//     const response = await fetch(url);
-
-//     if (response.ok) {
-//       const body = await response.json();
-//       const movie = body as IMovieDetail;
-//       displayMovie(movie);
-//     } else {
-//       throw new Error(response.status.toString());
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
